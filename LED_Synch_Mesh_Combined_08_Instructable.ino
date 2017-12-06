ab3380680b4e74834072ec7587f8b2c42629d5b6
Andre Geldenhuis <andregrey@gmail.com>
2017-12-07 00:01:52 +1300

    Predefined all functions for PlatformIO

--- LED_Synch_Mesh_Combined_08_Instructable.ino
+++ LED_Synch_Mesh_Combined_08_Instructable.ino
@@ -1,15 +1,15 @@
 /*
  *  LED_Synch_Mesh_Send
  *  Each ESP8266 based mesh node is a bar with 30 WS2812 ("Neopixel") LEDs
- *  
+ *
  *  The animations are synched between all the nodes
- *  
+ *
  *  Libraries used - standing on the back of giants - THANKS!
  *  painlessMesh
  *  FastLED
- *  
+ *
  *  2017 Carl F Sutter
- *  
+ *
  */
 
 // LED Setup - must come before the painlessMesh so CRGB can be used as a return type
@@ -45,104 +45,38 @@ unsigned long ul_Interval = 20UL;        // this is delay between animation step
 #define   MESH_PASSWORD   "foofoofoo"
 #define   MESH_PORT       5555
 
-void sendMessage(int display_step); // Prototype so PlatformIO doesn't complain
+// Forward declare functions - for PlatformIO compatability
+
+// this gets called when the designated controller sends a command to start a new animation
+// init any animation specific vars for the new mode, and reset the timer vars
+void receivedCallback( uint32_t from, String &msg );
+
+void newConnectionCallback(uint32_t nodeId);
+
+// this gets called when a node is added or removed from the mesh, so set the controller to the node with the lowest chip id
+void changedConnectionCallback();
+
+void nodeTimeAdjustedCallback(int32_t offset);
+
+// sort the given list of nodes
+void sortNodeList(SimpleList<uint32_t> &nodes);
+
+// check the timer and do one animation step if needed
+void stepAnimation();
+
+// send a broadcast message to all the nodes specifying the new animation mode for all of them
+void sendMessage(int display_step);
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+CRGB Wheel(byte WheelPos);
 
 painlessMesh  mesh;
 //Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
 
-
-// this gets called when the designated controller sends a command to start a new animation
-// init any animation specific vars for the new mode, and reset the timer vars
-void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("Setting display mode to %s. Received from %u\n", msg.c_str(), from);
-
-  // get the new display mode
-  display_mode = msg.toInt();
-
-  // init some vars that are the same for all animations
-  display_step = 0;
-  ul_PreviousMillis = 0UL; // make sure animation triggers on first step
-
-  // initialize any variables for the display_mode animation
-  switch (display_mode) {
-    case 1: // run the single color up the bar one LED at a time
-      color1 = CRGB(255, 0, 0);
-      ul_Interval = 50UL;
-      for(uint16_t i=0; i<NUM_LEDS; i++) leds[i] = CRGB(0, 255, 0); // set all the strips to green
-    break;
-
-    case 2: // rainbow colors
-      ul_Interval = 50UL;
-    break;
-
-    case 3: // go from dim to full on one color for all LEDs a few times
-      ul_Interval = 2UL;
-    break;
-
-    case 4: // alternate red and green LEDs
-      ul_Interval = 50UL;
-    break;
-
-    case 5: // animate a dot moving up all of them
-      ul_Interval = 50UL;
-    break;
-
-    case 6: // green bars with a red bar moving around them a few times
-      ul_Interval = 50UL;
-    break;
-
-    default:
-    break;
-  } // display_mode switch    
-} // receivedCallback
-
-
-void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-} // newConnectionCallback
-
-
-// this gets called when a node is added or removed from the mesh, so set the controller to the node with the lowest chip id
-void changedConnectionCallback() {
-  SimpleList<uint32_t> nodes;
-  uint32_t myNodeID = mesh.getNodeId();
-  uint32_t lowestNodeID = myNodeID;
-
-  Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
-
-  nodes = mesh.getNodeList();
-  Serial.printf("Num nodes: %d\n", nodes.size());
-  Serial.printf("Connection list:");
-
-  for (SimpleList<uint32_t>::iterator node = nodes.begin(); node != nodes.end(); ++node) {
-    Serial.printf(" %u", *node);
-    if (*node < lowestNodeID) lowestNodeID = *node;
-  }
-  Serial.println();
-
-  if (lowestNodeID == myNodeID) {
-    Serial.printf("Node %u: I am the controller now", myNodeID);
-    Serial.println();
-    amController = true;
-    // restart the current animation - to chatty - remove - better to wait for next animation
-    //sendMessage(display_mode);
-    //display_step = 0;
-    //ul_PreviousMillis = 0UL; // make sure animation triggers on first step
-  } else {
-    Serial.printf("Node %u is the controller now", lowestNodeID);
-    Serial.println();
-    amController = false;
-  }
-} // changedConnectionCallback
-
-void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-} // changedConnectionCallback
-
-
 void setup() {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  
+
   Serial.begin(115200);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
@@ -164,7 +98,7 @@ void setup() {
 
   // make this one the controller if there are no others on the mesh
   if (mesh.getNodeList().size() == 0) amController = true;
-  
+
 } // setup
 
 
@@ -185,7 +119,7 @@ void loop() {
         }
         break;
 
-      case 2: // rainbow colors 
+      case 2: // rainbow colors
         if (display_step > 255) {
           display_mode_changed = true;
           display_mode = 3;
@@ -236,6 +170,96 @@ void loop() {
 
 } // loop
 
+// all functions
+
+
+// this gets called when the designated controller sends a command to start a new animation
+// init any animation specific vars for the new mode, and reset the timer vars
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Setting display mode to %s. Received from %u\n", msg.c_str(), from);
+
+  // get the new display mode
+  display_mode = msg.toInt();
+
+  // init some vars that are the same for all animations
+  display_step = 0;
+  ul_PreviousMillis = 0UL; // make sure animation triggers on first step
+
+  // initialize any variables for the display_mode animation
+  switch (display_mode) {
+    case 1: // run the single color up the bar one LED at a time
+      color1 = CRGB(255, 0, 0);
+      ul_Interval = 50UL;
+      for(uint16_t i=0; i<NUM_LEDS; i++) leds[i] = CRGB(0, 255, 0); // set all the strips to green
+    break;
+
+    case 2: // rainbow colors
+      ul_Interval = 50UL;
+    break;
+
+    case 3: // go from dim to full on one color for all LEDs a few times
+      ul_Interval = 2UL;
+    break;
+
+    case 4: // alternate red and green LEDs
+      ul_Interval = 50UL;
+    break;
+
+    case 5: // animate a dot moving up all of them
+      ul_Interval = 50UL;
+    break;
+
+    case 6: // green bars with a red bar moving around them a few times
+      ul_Interval = 50UL;
+    break;
+
+    default:
+    break;
+  } // display_mode switch
+} // receivedCallback
+
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+} // newConnectionCallback
+
+
+// this gets called when a node is added or removed from the mesh, so set the controller to the node with the lowest chip id
+void changedConnectionCallback() {
+  SimpleList<uint32_t> nodes;
+  uint32_t myNodeID = mesh.getNodeId();
+  uint32_t lowestNodeID = myNodeID;
+
+  Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+
+  nodes = mesh.getNodeList();
+  Serial.printf("Num nodes: %d\n", nodes.size());
+  Serial.printf("Connection list:");
+
+  for (SimpleList<uint32_t>::iterator node = nodes.begin(); node != nodes.end(); ++node) {
+    Serial.printf(" %u", *node);
+    if (*node < lowestNodeID) lowestNodeID = *node;
+  }
+  Serial.println();
+
+  if (lowestNodeID == myNodeID) {
+    Serial.printf("Node %u: I am the controller now", myNodeID);
+    Serial.println();
+    amController = true;
+    // restart the current animation - to chatty - remove - better to wait for next animation
+    //sendMessage(display_mode);
+    //display_step = 0;
+    //ul_PreviousMillis = 0UL; // make sure animation triggers on first step
+  } else {
+    Serial.printf("Node %u is the controller now", lowestNodeID);
+    Serial.println();
+    amController = false;
+  }
+} // changedConnectionCallback
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+} // changedConnectionCallback
 
 // sort the given list of nodes
 void sortNodeList(SimpleList<uint32_t> &nodes) {
@@ -249,12 +273,12 @@ void sortNodeList(SimpleList<uint32_t> &nodes) {
     for (SimpleList<uint32_t>::iterator node = nodes.begin(); node != nodes.end(); ++node) {
       if (*node < *smallest_node) smallest_node = node;
     }
-    
+
     // add it to the sorted list and remove it from the old list
     nodes_sorted.push_back(*smallest_node);
     nodes.erase(smallest_node);
   } // while
-  
+
   // copy the sorted list back into the now empty nodes list
   for (SimpleList<uint32_t>::iterator node = nodes_sorted.begin(); node != nodes_sorted.end(); ++node) nodes.push_back(*node);
 } // sortNodeList
@@ -263,7 +287,7 @@ void sortNodeList(SimpleList<uint32_t> &nodes) {
 // check the timer and do one animation step if needed
 void stepAnimation() {
   // if the animation delay time has past, run another animation step
-  unsigned long ul_CurrentMillis = millis();  
+  unsigned long ul_CurrentMillis = millis();
   if (ul_CurrentMillis - ul_PreviousMillis > ul_Interval) {
     ul_PreviousMillis = millis();
     switch (display_mode) {
@@ -298,7 +322,7 @@ void stepAnimation() {
         FastLED.show();
       break;
 
-      case 5: // animate a dot moving up all of them 
+      case 5: // animate a dot moving up all of them
       {
         int curr_strip_num = display_step / NUM_LEDS;
         SimpleList<uint32_t> nodes = mesh.getNodeList();
@@ -308,7 +332,7 @@ void stepAnimation() {
         // add this node to the node list and sort the list
         nodes.push_back(myNodeID);
         sortNodeList(nodes);
-        
+
         // blacken all the leds
         for(uint16_t i=0; i<NUM_LEDS; i++) leds[i] = CRGB(0, 0, 0);
 
@@ -351,7 +375,6 @@ void stepAnimation() {
         FastLED.show();
       }
       break;
-      
     } // display_mode switch
 
     display_step++;
@@ -384,4 +407,3 @@ CRGB Wheel(byte WheelPos) {
   WheelPos -= 170;
   return CRGB(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
-
